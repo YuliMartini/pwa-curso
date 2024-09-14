0e16dc46a1e9ea323c6f3e3ba88ae3c1bbb6c5da
Giuliana Martini <yulimartini94@gmail.com>
2024-09-14 13:23:07 -0300

    4 - service worker y fetch event

---
+++ 02-service-worker/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("fetch", (event) => {
+  const resp = fetch(event.request).then((resp) => {
+    return resp.ok ? resp : fetch("img/main.jpg");
+  });
+
+  event.respondWith(resp);
+});
